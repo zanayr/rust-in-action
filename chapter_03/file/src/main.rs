69308fbc0e2bcad9ade1ed26b3687d7db997d0a0
Ryan Fickenscher <rsficken@gmail.com>
2022-08-28 22:28:12 -0700

    chapter 03, listing 3.3

--- chapter_03/file/src/main.rs
+++ chapter_03/file/src/main.rs
@@ -1,15 +1,44 @@
+#![allow(unused_variables)]
+
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
 }
 
-fn main() {
-    let file = File { name: String::from("readme.md"), data: Vec::new() };
+fn open(f: &mut File) -> bool {
+    true
+}
 
-    let file_name = &file.name;
-    let file_length = &file.data.len();
+fn close(f: &mut File) -> bool {
+    true
+}
+
+fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
+}
+
+fn main() {
+    let mut file = File {
+        name: String::from("readme.md"),
+        data: vec![114, 117, 115, 116, 33],
+    };
+
+    let mut buffer: Vec<u8> = vec![];
+
+
+    open(&mut file);
+    let file_length = read(&file, &mut buffer);
+    close(&mut file);
+
+    let text = String::from_utf8_lossy(&buffer);
 
     println!("{:?}", file);
-    println!("{} is {} bytes long", file_name, file_length);
+    println!("{} is {} bytes long", &file.name, file_length);
+    println!("{}", text);
 }
