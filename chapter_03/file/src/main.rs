91069f2c8cbe0cd6ae56d24b09ae0bef890f42a8
Ryan Fickenscher <rsficken@gmail.com>
2022-08-28 23:19:04 -0700

    chapter 03, listing 3.8

--- chapter_03/file/src/main.rs
+++ chapter_03/file/src/main.rs
@@ -1,4 +1,8 @@
-#![allow(unused_variables)]
+use rand::prelude::*;
+
+fn one_in(denominator: u32) -> bool {
+    thread_rng().gen_ratio(1, denominator)
+}
 
 #[derive(Debug)]
 struct File {
@@ -20,22 +24,30 @@ impl File {
         file
     }
 
-    fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
         let mut tmp = self.data.clone();
         let read_length = tmp.len();
 
         save_to.reserve(read_length);
         save_to.append(&mut tmp);
-        read_length
+        Ok(read_length)
     }
 }
 
-fn open(file: &mut File) -> bool {
-    true
+fn open(file: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Interrupted by signal!");
+        return Err(err_msg);
+    }
+    Ok(file)
 }
 
-fn close(file: &mut File) -> bool {
-    true
+fn close(file: File) -> Result<File, String> {
+    if one_in(100_000) {
+        let err_msg = String::from("Interrupted by signal!");
+        return Err(err_msg);
+    }
+    Ok(file)
 }
 
 fn main() {
@@ -45,9 +57,9 @@ fn main() {
     let mut file = File::new_with_data("readme.md", &file_data);
 
     let mut buffer: Vec<u8> = vec![];
-    open(&mut file);
-    let file_length = file.read(&mut buffer);
-    close(&mut file);
+    file = open(file).unwrap();
+    let file_length = file.read(&mut buffer).unwrap();
+    file = close(file).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
