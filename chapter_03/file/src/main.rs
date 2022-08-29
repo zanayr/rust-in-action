6fdf092d9f37c730efa10b88859f216951c7c537
Ryan Fickenscher <rsficken@gmail.com>
2022-08-28 22:17:03 -0700

    chapter 03, listing 3.2

--- chapter_03/file/src/main.rs
+++ chapter_03/file/src/main.rs
@@ -1,23 +1,15 @@
-#![allow(unused_variables)]
-
-type File = String;
-
-fn open(f: &mut File) -> bool {
-    true
-}
-
-fn close(f: &mut File) -> bool {
-    true
-}
-
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
 }
 
 fn main() {
-    let mut f1 = File::from("readme.md");
-    open(&mut f1);
-    // read(f1, vec![]);
-    close(&mut f1);
+    let file = File { name: String::from("readme.md"), data: Vec::new() };
+
+    let file_name = &file.name;
+    let file_length = &file.data.len();
+
+    println!("{:?}", file);
+    println!("{} is {} bytes long", file_name, file_length);
 }
