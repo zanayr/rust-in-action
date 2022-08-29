cb6324828249d4b9b4274553319b80d2ac887cc4
Ryan Fickenscher <rsficken@gmail.com>
2022-08-28 22:40:12 -0700

    chapter 03, listing 3.4

--- chapter_03/file/src/main.rs
+++ chapter_03/file/src/main.rs
@@ -6,34 +6,47 @@ struct File {
     data: Vec<u8>,
 }
 
-fn open(f: &mut File) -> bool {
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut file = File::new(name);
+        file.data = data.clone();
+        file
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+}
+
+fn open(file: &mut File) -> bool {
     true
 }
 
-fn close(f: &mut File) -> bool {
+fn close(file: &mut File) -> bool {
     true
-}
-
-fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
-    let mut tmp = f.data.clone();
-    let read_length = tmp.len();
-
-    save_to.reserve(read_length);
-    save_to.append(&mut tmp);
-    read_length
 }
 
 fn main() {
-    let mut file = File {
-        name: String::from("readme.md"),
-        data: vec![114, 117, 115, 116, 33],
-    };
+    let file_data: Vec<u8> = vec![
+        114, 117, 115, 116, 33,
+    ];
+    let mut file = File::new_with_data("readme.md", &file_data);
 
     let mut buffer: Vec<u8> = vec![];
-
-
     open(&mut file);
-    let file_length = read(&file, &mut buffer);
+    let file_length = file.read(&mut buffer);
     close(&mut file);
 
     let text = String::from_utf8_lossy(&buffer);
