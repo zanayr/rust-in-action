66ff971a1aca7f31eb77f13a11c754e75f361c02
Ryan Fickenscher <rsficken@gmail.com>
2022-09-03 22:35:43 -0700

    chapter 05, cpu Listing 5.29

--- chapter_05/cpu/src/main.rs
+++ chapter_05/cpu/src/main.rs
@@ -2,6 +2,8 @@ struct CPU {
     registers: [u8; 16],
     position_in_memory: usize,
     memory: [u8; 0x1000],
+    stack: [u16; 16], // maximum stack size 16
+    stack_pointer: usize, // giving the pointer a usize makes it easier to index values
 }
 
 impl CPU {
@@ -23,22 +25,50 @@ impl CPU {
             let y = ((opcode & 0x00F0) >>  4) as u8;
             let d = ((opcode & 0x000F) >>  0) as u8;
 
+            let nnn = opcode & 0x0FFF;
+            // let kk  = (opcode & 0x00FF) as u8;
+
             match (c, x, y, d) {
-                (0, 0, 0, 0)     => { return; }, // opcode 0x0000 terminates execution
-                (0x8, _, _, 0x4) => self.add_xy(x, y),
-                _                => todo!("opcode {:04x}", opcode),
+                (  0,   0,   0,   0) => { return; }, // opcode 0x0000 terminates execution
+                (  0,   0, 0xE, 0xE) => self.ret(),
+                (0x2,   _,   _,   _) => self.call(nnn),
+                (0x8,   _,   _, 0x4) => self.add_xy(x, y),
+                _                    => todo!("opcode {:04x}", opcode),
             }
         }
+    }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow!")
+        }
+
+        stack[sp] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow")
+        }
+
+        self.stack_pointer -= 1;
+        let addr = self.stack[self.stack_pointer];
+        self.position_in_memory = addr as usize;
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
         let arg1 = self.registers[x as usize];
         let arg2 = self.registers[y as usize];
 
-        let (val, overflow) = arg1.overflowing_add(arg2);
+        let (val, overflow_detected) = arg1.overflowing_add(arg2);
         self.registers[x as usize] = val;
 
-        if overflow {
+        if overflow_detected {
             self.registers[0xF] = 1;
         } else {
             self.registers[0xF] = 0;
@@ -51,21 +81,25 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10; // initialize a few
-    cpu.registers[3] = 10; // registers with values
 
     let mem = &mut cpu.memory;
-    mem[0] = 0x80; mem[1] = 0x14; // loads opcode 0x8014, adding register 1 to register 0
-    mem[2] = 0x80; mem[3] = 0x24; // loads opcode 0x8024, adding register 2 to register 0
-    mem[4] = 0x80; mem[5] = 0x34; // loads opcode 0x8034, adding register 3 to register 0
+    mem[0x000] = 0x21; mem[0x001] = 0x00; // 0x2_100 | CALL 0x100
+    mem[0x002] = 0x21; mem[0x003] = 0x00; // 0x2_100 | CALL 0x100
+    mem[0x004] = 0x00; mem[0x005] = 0x00; // 0x00_00 | EXIT
+    // [0x006] ... [0x099]
+    mem[0x100] = 0x80; mem[0x101] = 0x14; // 0x80_14 | ADD register 1 to register 0
+    mem[0x102] = 0x80; mem[0x103] = 0x14; // 0x80_14 | ADD register 1 to register 0
+    mem[0x104] = 0x00; mem[0x105] = 0xEE; // 0x00_EE | RETURN
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
 
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
