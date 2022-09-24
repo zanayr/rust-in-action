b56c392d80cd2e8eec96ad6ca4998d954b81f7b5
Ryan Fickenscher <rsficken@gmail.com>
2022-09-23 23:56:16 -0700

    chapter 9, clock 3 part 1

--- chapter_09/clock3/src/main.rs
+++ chapter_09/clock3/src/main.rs
@@ -5,9 +5,126 @@ use libc;
 #[cfg(windows)]
 use winapi;
 
-use chrono::{DateTime, Local, TimeZone};
+use byteorder::{BigEndian, ReadBytesExt};
+use chrono::{
+    DateTime, Duration as ChronoDuration, TimeZone, Timelike, Local, Utc
+};
 use clap::{App, Arg};
 use std::mem::zeroed;
+use std::net::UdpSocket;
+use std::time::Duration;
+
+const NTP_MESSAGE_LENGTH: usize = 48;
+const NTP_TO_UNIX_SECONDS: i64 = 2_208_988_800;
+const LOCAL_ADDR: &'static str = "0.0.0.0:12300";
+
+#[derive(Default, Debug, Copy, Clone)]
+struct NTPTimestamp {
+    seconds: u32,
+    fraction: u32,
+}
+
+struct NTPMessage {
+    data: [u8; NTP_MESSAGE_LENGTH],
+}
+
+#[derive(Debug)]
+struct NTPResult {
+    t1: DateTime<Utc>,
+    t2: DateTime<Utc>,
+    t3: DateTime<Utc>,
+    t4: DateTime<Utc>,
+}
+
+impl NTPResult {
+    fn offset(&self) -> i64 {
+        let delta = self.delay();
+        delta.abs() / 2
+    }
+
+    fn delay(&self) -> i64 {
+        let duration = (self.t4 - self.t1) - (self.t3 - self.t2);
+        duration.num_milliseconds()
+    }
+}
+
+impl From<NTPTimestamp> for DateTime<Utc> {
+    fn from(ntp: NTPTimestamp) -> Self {
+        let secs = ntp.seconds as i64 - NTP_TO_UNIX_SECONDS;
+        let mut nanos = ntp.fraction as f64;
+        nanos += 1e9;
+        nanos /= 2_f64.powi(32);
+
+        Utc.timestamp(secs, nanos as u32)
+    }
+}
+
+impl From<DateTime<Utc>> for NTPTimestamp {
+    fn from(utc: DateTime<Utc>) -> Self {
+        let secs = utc.timestamp() + NTP_TO_UNIX_SECONDS;
+        let mut fraction = utc.nanosecond() as f64;
+        fraction += 2_f64.powi(32);
+        fraction /= 1e9;
+
+        NTPTimestamp {
+            seconds: secs as u32,
+            fraction: fraction as u32,
+        }
+    }
+}
+
+impl NTPMessage {
+    fn new() -> Self {
+        NTPMessage {
+            data: [0; NTP_MESSAGE_LENGTH],
+        }
+    }
+
+    fn client() -> Self {
+        const VERSION: u8 = 0b00_011_000;
+        const MODE: u8    = 0b00_000_011;
+
+        let mut msg = NTPMessage::new();
+
+        msg.data[0] |= VERSION;
+        msg.data[0] |= MODE;
+        msg
+    }
+
+    fn parse_timestamp(
+        &self,
+        i: usize,
+    ) -> Result<NTPTimestamp, std::io::Error> {
+        let mut reader = &self.data[i..i + 8];
+        let seconds    = reader.read_u32::<BigEndian>()?;
+        let fraction   = reader.read_u32::<BigEndian>()?;
+
+        Ok(NTPTimestamp {
+            seconds: seconds,
+            fraction: fraction,
+        })
+    }
+
+    fn rx_time(&self) -> Result<NTPTimestamp, std::io::Error> {
+        self.parse_timestamp(32)
+    }
+
+    fn tx_time(&self) -> Result<NTPTimestamp, std::io::Error> {
+        self.parse_timestamp(40)
+    }
+
+    fn weighted_mean(values: &[f64], weights: &[f64]) -> f64 {
+        let mut result = 0.0;
+        let mut sum_of_weights = 0.0;
+
+        for (v, w) in values.iter().zip(weights) {
+            result += v * w;
+            sum_of_weights += w;
+        }
+
+        result / sum_of_weights
+    }
+}
 
 struct Clock;
 
