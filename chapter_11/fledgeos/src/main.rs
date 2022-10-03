f0021b3e988a9f69e658b69437f55688fe797816
Ryan Fickenscher <rsficken@gmail.com>
2022-10-02 20:24:52 -0700

    chapter 11, fledgeos v3

--- chapter_11/fledgeos/src/main.rs
+++ chapter_11/fledgeos/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(core_intrinsics)]
+#![feature(land_items)]
 
 use core::intrinsics;
 use core::panic::PanicInfo;
@@ -11,6 +12,10 @@ use x86_64::instructions::{hlt};
 pub fn panic(_info: &PanicInfo) -> ! {
     intrinsics::abort();
 }
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern "C" fn en_personality() { }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
