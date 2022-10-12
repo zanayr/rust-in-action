664a948af3edb4ff160d57de7320d2ead22a03e8
Ryan Fickenscher <rsficken@gmail.com>
2022-10-11 19:54:30 -0700

    chapter 12, listing 12.12, ignore

---
+++ chapter_12/ignore/src/main.rs
@@ -0,0 +1,18 @@
+use libc::{ signal, raise };
+use libc::{ SIG_DFL, SIG_IGN, SIGTERM };
+
+fn main() {
+    unsafe {
+        signal(SIGTERM, SIG_IGN);
+        raise(SIGTERM);
+    }
+
+    println!("ok");
+
+    unsafe {
+        signal(SIGTERM, SIG_DFL);
+        raise(SIGTERM);
+    }
+
+    println!("not ok");
+}
